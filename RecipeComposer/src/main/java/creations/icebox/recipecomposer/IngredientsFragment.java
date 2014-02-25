f760f70a7de9a9f83b6ce62ccfdeefdd59a3a117
trance-with-me <amb@sonic.net>
2014-02-25 14:35:54 -0800

    Separate the AsyncTask out from RecipeComposerActivity

--- RecipeComposer/src/main/java/creations/icebox/recipecomposer/IngredientsFragment.java
+++ RecipeComposer/src/main/java/creations/icebox/recipecomposer/IngredientsFragment.java
@@ -15,7 +15,6 @@ import android.widget.TextView;
 public class IngredientsFragment extends Fragment {
 
     private static final String TAG = "***INGREDIENTS FRAGMENT: ";
-    private String sample_ingredients[];
 
     public static IngredientsFragment newInstance() {
         Log.d(TAG, "newInstance");
@@ -23,23 +22,12 @@ public class IngredientsFragment extends Fragment {
     }
 
     public IngredientsFragment() {
-        sample_ingredients = new String[] {
-                "Jelly Beans",
-                "Ice cream Sandwiches",
-                "HoneyCombs"
-        };
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView");
-//        ListAdapter listAdapter = new ArrayAdapter<String>(inflater.getContext(),
-//                android.R.layout.simple_list_item_1, sample_ingredients);
-//        setListAdapter(listAdapter);
-
-//        return super.onCreateView(inflater, container, savedInstanceState);
-
         View rootView = inflater.inflate(R.layout.fragment_ingredients, container, false);
         return rootView;
     }
@@ -63,92 +51,3 @@ public class IngredientsFragment extends Fragment {
     }
 
 }
-
-//    // TODO: Rename parameter arguments, choose names that match
-//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-//
-//    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
-//
-//    private OnFragmentInteractionListener mListener;
-//
-//    /**
-//     * Use this factory method to create a new instance of
-//     * this fragment using the provided parameters.
-//     *
-//     * @param param1 Parameter 1.
-//     * @param param2 Parameter 2.
-//     * @return A new instance of fragment IngredientsFragment.
-//     */
-//    // TODO: Rename and change types and number of parameters
-//    public static IngredientsFragment newInstance(String param1, String param2) {
-//        IngredientsFragment fragment = new IngredientsFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-//    public IngredientsFragment() {
-//        // Required empty public constructor
-//    }
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_ingredients, container, false);
-//    }
-//
-//    // TODO: Rename method, update argument and hook method into UI event
-//    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
-//        }
-//    }
-//
-//    @Override
-//    public void onAttach(Activity activity) {
-//        super.onAttach(activity);
-//        try {
-//            mListener = (OnFragmentInteractionListener) activity;
-//        } catch (ClassCastException e) {
-//            throw new ClassCastException(activity.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-//
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
-//
-//    /**
-//     * This interface must be implemented by activities that contain this
-//     * fragment to allow an interaction in this fragment to be communicated
-//     * to the activity and potentially other fragments contained in that
-//     * activity.
-//     * <p>
-//     * See the Android Training lesson <a href=
-//     * "http://developer.android.com/training/basics/fragments/communicating.html"
-//     * >Communicating with Other Fragments</a> for more information.
-//     */
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        public void onFragmentInteraction(Uri uri);
-//    }
-//
-//}
